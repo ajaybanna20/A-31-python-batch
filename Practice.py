6ed7602898eed505f6942c61371b24bc200bb74d
AJAY SINGH <ajayshekhawat990@gmail.com>
2026-08-16 21:46:53 +0530

    Add files via upload

---
+++ Practice.py
@@ -0,0 +1,67 @@
+
+# dict = {
+#     "table" : ["a peace of furniture ", "list of fact & figures"],
+#     "cat " : "a small animal ",
+# }
+# print (dict)
+
+# sub = {"python","java","c++","python","javascript","java","python","java","c++","c",}
+
+# print((sub))
+
+# marks = {}
+
+# phy = int (input("enter phy marks:"))
+# marks.update({"physics": phy})
+
+# chem = int (input("enter chem marks:"))
+# marks.update({"chem": phy})
+
+# math = int (input("enter math marks:"))
+# marks.update({"math": phy})
+
+
+# print(marks)
+
+# age = {9,"9.0"}
+
+# print(age)
+
+# num = (1,4,9,16,25,36,49,64,81,100)
+# x=9
+# idx=0
+# for el in num:
+#     if (el ==x):
+#         print ("found at idx",idx)
+#     idx += 1
+
+#sum of n (while loop)
+# n =7
+# sum = 0
+# i=1
+# while i <= n:
+#     sum+=i
+#     i+=1
+  
+
+# print ("sum of n:",sum)
+
+#factorial of n (while loop)
+# n =5
+# fact= 1
+# i=1
+# while i <= n:
+#     fact *=i
+#     i+=1
+
+# print ("factorial of n:",fact)
+
+#factorial of n (for loop)
+n=5
+
+fact = 1
+
+for i in range(1,n+1):
+    fact*= i
+
+print("factorial of n",fact)
